c10556bafd8f790bf3a91e6e250a379ca89512b4
Nick <nickgulya@gmail.com>
2018-03-19 19:48:35 +0200

    change redirect after save object, adding functionality for added list of shifts for chosen date, change color of markers, fixed fill in button, change redirect after fill in vacancy

---
+++ src/app/shared/components/spinner/spinner.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'spinner',
+  template: `
+    <div class="lds-spinner">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+  </div>
+  `
+})
+
+export class SpinnerComponent {}
