19ce72b8e3a17549bba50337c7c2160d380d26f5
Nick <nickgulya@gmail.com>
2018-05-14 10:11:08 +0300

    RV-633 - fixed formula of plan calculation

--- src/app/billing/components/billing-plan/billing-plan.component.ts
+++ src/app/billing/components/billing-plan/billing-plan.component.ts
@@ -27,41 +27,47 @@ export class BillingPlanComponent implements OnInit {
     this.workerCount = this.currentPlan ? this.currentPlan.worker_count : 1;
     this.plans = [
       {
+        id: 1,
         name: 'Cancel anytime',
         type: 'monthly',
         description: 'Monthly',
         save: false,
         pay: 13,
-        price: 11,
+        procent: 1,
         active: this.currentPlan && this.currentPlan.type === 'monthly',
         start: 120
       },
       {
+        id: 2,
         name: 'Annual plan',
         type: 'annual',
         description: 'Annually',
         save: true,
         pay: 10,
-        price: 9,
+        procent: 0.75,
         active: this.currentPlan && this.currentPlan.type === 'annual',
         start: 90
       }
     ];
   }
 
-  public planPay(price: number, start: number): number {
-    return this.workerCount > 10 ? start + (this.workerCount - 10) * price : start;
+  public planPay(plan: Plan, procent?: number): number {
+    const price = 120 + (this.workerCount - 10) * 11;
+
+    return this.workerCount > 10 ? Math.round(price * procent) : plan.start;
   }
 
-  public planPayYear(price: number, start: number): number {
-    return this.planPay(price, start) * 12;
+  public planPayYear(plan: Plan, procent?: number): number {
+    const price = this.planPay(plan, 1);
+
+    return Math.round(price * 12 * procent);
   }
 
   public selectPlan(plan) {
     const body = {
       type: plan.type,
       worker_count: this.workerCount,
-      price: this.planPay(plan.price, plan.start)
+      price: plan.id === 1 ? this.planPay(plan) : this.planPayYear(plan, plan.procent)
     };
 
     this.selectedPlan.emit(body);
--- src/app/billing/models/plan.ts
+++ src/app/billing/models/plan.ts
@@ -1,12 +1,13 @@
 export interface Plan {
+  id: number;
   name?: string;
   type?: string;
   description?: string;
   save?: boolean;
   pay?: number;
-  price?: number;
   active?: boolean;
   start?: number;
   status?: string;
   worker_count?: number;
+  procent?: number;
 }
