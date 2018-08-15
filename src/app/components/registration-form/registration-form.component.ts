dd71cab030633aa3d36c43d7edbc4b37e242d01a
Nick <nickgulya@gmail.com>
2018-08-15 11:55:16 +0300

    RV-132 - some changes

--- src/app/components/registration-form/registration-form.component.ts
+++ src/app/components/registration-form/registration-form.component.ts
@@ -1,21 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
+import { Field } from '../../dynamic-form/models/field.model';
+
 @Component({
   selector: 'registration-form',
   templateUrl: 'registration-form.component.html',
   styleUrls: ['./registration-form.component.scss']
 })
 export class RegistrationFormComponent implements OnInit {
-
   public settings: any;
-  public config: any;
+  public config: Field[];
   public password: boolean;
 
-  constructor(
-    private route: ActivatedRoute,
-    private router: Router
-  ) { }
+  constructor(private route: ActivatedRoute, private router: Router) {}
 
   public ngOnInit() {
     this.settings = this.route.snapshot.data['settings'];
@@ -30,7 +28,26 @@ export class RegistrationFormComponent implements OnInit {
     // });
   }
 
-  public setFormConfig(config: any) {
+  public setFormConfig(config: Field[]) {
     this.config = config;
+  }
+
+  public updateConfig(config: Field[]) {
+    const streetAddress = config.find(field =>
+      field.key.includes('street_address')
+    );
+
+    if (streetAddress) {
+      config.forEach(field => {
+        if (
+          field.key.includes('postal_code') ||
+          field.key.includes('state') ||
+          field.key.includes('country') ||
+          field.key.includes('city')
+        ) {
+          console.log(field.key);
+        }
+      });
+    }
   }
 }
