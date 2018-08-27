d74006e31fe691d98beaf36a401f5d7e0d00929a
Nick <nickgulya@gmail.com>
2018-08-27 17:01:13 +0300

    RV-714 - added information about last shift

--- src/app/dynamic-form/components/extend/extend.component.ts
+++ src/app/dynamic-form/components/extend/extend.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+
+import { Subscription } from 'rxjs/Subscription';
 
 import { Field } from '../../models/field.model';
 
@@ -8,11 +10,15 @@ import { Field } from '../../models/field.model';
   templateUrl: './extend.component.html',
   styleUrls: ['./extend.component.scss']
 })
-export class ExtendComponent implements OnInit {
+export class ExtendComponent implements OnInit, OnDestroy {
 
   public config: Field;
   public viewData: FormGroup;
   public viewConfig: { [key: string]: Field };
+  public formData: any;
+  public autoFillData: any;
+
+  private formSubscription: Subscription;
 
   constructor(
     private fb: FormBuilder
@@ -38,16 +44,34 @@ export class ExtendComponent implements OnInit {
         key: 'extendCandidates',
         type: 'checkbox',
         templateOptions: {
-          label: 'Candidates',
-          description: 'Some help text'
+          label: 'Candidates'
         }
       }
     };
 
+    this.checkFormData();
+  }
+
+  public ngOnDestroy() {
+    if (this.formSubscription) {
+      this.formSubscription.unsubscribe();
+    }
   }
 
   public eventHandler(e) {
     console.log(e);
+  }
+
+  public checkFormData() {
+    if (this.config.formData) {
+      const subscription = this.config.formData.subscribe((data) => {
+        this.formData = data.data;
+
+        this.autoFillData = this.formData.last_fullfilled;
+      });
+
+      this.formSubscription = subscription;
+    }
   }
 
 }
