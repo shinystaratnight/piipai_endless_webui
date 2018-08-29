a372ebb4ade6783e1f809aa092f5ba78e6659754
Nick <nickgulya@gmail.com>
2018-08-29 12:09:52 +0300

    RV-274 - change api key

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -58,6 +58,6 @@ export const ENV_PROVIDERS = [
 ];
 
 export const environment = {
-  GOOGLE_GEO_CODING_API_KEY: 'AIzaSyB0-XgGs0g5awE_VtUh-_oHlU0nzhy4oOM',
+  GOOGLE_GEO_CODING_API_KEY: 'AIzaSyA7GZF8hAU3OEziHEJmKsJC_ccywNBwllg',
   STRIPE_PUBLIC_API_KEY: 'pk_test_d5AKTy7WjvRJBJ9wZduzAFjI'
 };
