7905adcd8c19b7f8c479583463c9f3b567104868
Nick <nickgulya@gmail.com>
2018-10-03 10:30:35 +0300

    RV-682 - fix auth guard

--- src/app/guards/auth-guard.ts
+++ src/app/guards/auth-guard.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router, ActivatedRouteSnapshot } from '@angular/router';
 
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/observable/combineLatest';
-import 'rxjs/add/operator/do';
+import { Observable, combineLatest, of } from 'rxjs';
+import { tap, mergeMap, catchError } from 'rxjs/operators';
 
 import { UserService, User, NavigationService, Role } from '../services/';
 import { CheckPermissionService } from '../shared/services/';
@@ -20,31 +19,37 @@ export class AuthGuard implements CanActivate {
 
   public canActivate(
     route: ActivatedRouteSnapshot
-  ): Observable<boolean> {
-    return this.userServise.getUserData()
-      .mergeMap((user: User) => {
-        return Observable.combineLatest(
-          Observable.of(user),
-          this.navigationService.getPages(user.currentRole)
-        );
-      })
-      .mergeMap((response: any) => {
-        const role: Role = response[0].currentRole;
+  ) {
+    return this.userServise
+      .getUserData()
+      .pipe(
+        mergeMap((user: User) => {
+          return combineLatest(
+            of(user),
+            this.navigationService.getPages(user.currentRole)
+          );
+        }),
+        mergeMap((response: any) => {
+          const role: Role = response[0].currentRole;
 
-        if (role.__str__.includes('manager')) {
-          return this.checkPermissionServise.checkPermission(
-              response[0].data.user,
-              (<any> route)._urlSegment.segments,
-              response[1]
-            )
-            .do((res: boolean) => !res && this.router.navigate(['login']));
-        } else {
-          return Observable.of(true);
-        }
-      })
-      .catch((err: any) => {
-        this.router.navigate(['login']);
-        return Observable.of(false);
-      });
+          if (role.__str__.includes('manager')) {
+            return this.checkPermissionServise
+              .checkPermission(
+                response[0].data.user,
+                (<any> route)._urlSegment.segments,
+                response[1]
+              )
+              .pipe(
+                tap((res: boolean) => !res && this.router.navigate(['login']))
+              );
+          } else {
+            return of(true);
+          }
+        }),
+        catchError(() => {
+          this.router.navigate(['login']);
+          return of(false);
+        })
+      );
   }
 }
