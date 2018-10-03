9ee7606ab390690903aa23218b530934ef21edb6
Nick <nickgulya@gmail.com>
2018-10-03 10:51:34 +0300

    RV-682 - fix form builder service

--- src/app/dynamic-form/services/form-builder.service.ts
+++ src/app/dynamic-form/services/form-builder.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers } from '@angular/http';
 
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/catch';
+import { map, catchError } from 'rxjs/operators';
 
 import { ErrorsService } from '../../shared/services/errors.service';
 
@@ -18,20 +17,29 @@ export class FormBuilderService {
   ) { }
 
   public getRenderData(id: string) {
-    return this.http.get(this.formEndpoint + id + '/render/')
-      .map((res: any) => res.json())
-      .catch((error: any) => this.errorsService.parseErrors(error));
+    return this.http
+      .get(this.formEndpoint + id + '/render/')
+      .pipe(
+        map((res: any) => res.json()),
+        catchError((error: any) => this.errorsService.parseErrors(error))
+      );
   }
 
   public sendFormData(id: string, data: any) {
-    return this.http.post(this.formEndpoint + id + '/submit/', data)
-      .map((res: any) => res.json())
-      .catch((error: any) => this.errorsService.parseErrors(error));
+    return this.http
+      .post(this.formEndpoint + id + '/submit/', data)
+      .pipe(
+        map((res: any) => res.json()),
+        catchError((error: any) => this.errorsService.parseErrors(error))
+      );
   }
 
   public parseAddress(data) {
-    return this.http.post(this.parseAddressEndpoint, data)
-      .map((res: any) => res.json())
-      .catch((error: any) => this.errorsService.parseErrors(error));
+    return this.http
+      .post(this.parseAddressEndpoint, data)
+      .pipe(
+        map((res: any) => res.json()),
+        catchError((error: any) => this.errorsService.parseErrors(error))
+      );
   }
 }
