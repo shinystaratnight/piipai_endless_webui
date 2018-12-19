01ce71bd8cf4c759fb4b9138d7cfa1af236473e5
Nick <nickgulya@gmail.com>
2018-12-19 17:15:23 +0200

    RV-701 - change api url for staging server

--- src/environments/environment.stage.ts
+++ src/environments/environment.stage.ts
@@ -2,6 +2,6 @@ export const environment = {
   GOOGLE_GEO_CODING_API_KEY: 'AIzaSyB0-XgGs0g5awE_VtUh-_oHlU0nzhy4oOM',
   STRIPE_PUBLIC_API_KEY: 'pk_test_d5AKTy7WjvRJBJ9wZduzAFjI',
   production: true,
-  api: 'http://api.r3sourcertest.com',
+  api: 'https://api.r3sourcertest.com',
   clientId: 'X4DfjpKt4jkM56QEgeYgYjP6OCxbf0WQH0oh0hXB'
 };
