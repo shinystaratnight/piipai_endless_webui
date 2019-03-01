2c8cf65d5c7d5cd60024e52f4038830b2c6a2b87
Nick <nickgulya@gmail.com>
2019-03-01 13:24:49 +0200

    RV-633 - change stripe key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   GOOGLE_GEO_CODING_API_KEY: 'AIzaSyB0-XgGs0g5awE_VtUh-_oHlU0nzhy4oOM',
-  STRIPE_PUBLIC_API_KEY: 'pk_test_d5AKTy7WjvRJBJ9wZduzAFjI',
+  STRIPE_PUBLIC_API_KEY: 'pk_test_2MvNXfKY5LWSSkOz241aYEs4',
   production: true,
   api: 'https://api.r3sourcer.com',
   clientId: 'GsZ3nrBWIFYocR2EIPJInfvlccT3Stk2NOq5cqAK'
