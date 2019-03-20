ade6a1ed28913e57b99400866cacc44d4f3bf440
Nick <nickgulya@gmail.com>
2019-03-20 11:18:53 +0200

    RV-838 - add invoice filters

--- src/app/dynamic-form/models/filters/index.ts
+++ src/app/dynamic-form/models/filters/index.ts
@@ -1,12 +1,13 @@
 import { RangeFilter, RangeFilterOptions, Range } from './range-filter';
 import { RelatedFilter, RelatedFilterOptions, Related } from './related-filter';
+import { DateFilter, DateFilterOptions, Date } from './date-filter';
 
 export * from './range-filter';
 export * from './related-filter';
 
-export type Filter = RangeFilter | RelatedFilter;
-export enum Type { Range, Relared }
-export type FilterOptions = RangeFilterOptions | RelatedFilterOptions;
+export type Filter = RangeFilter | RelatedFilter | DateFilter;
+export enum Type { Range, Relared, Date }
+export type FilterOptions = RangeFilterOptions | RelatedFilterOptions | DateFilterOptions;
 
 export function createFilter(type: Type, options: FilterOptions): Filter {
   switch (type) {
@@ -15,6 +16,9 @@ export function createFilter(type: Type, options: FilterOptions): Filter {
 
     case Type.Relared:
       return new RelatedFilter(options as RelatedFilterOptions);
+
+    case Type.Date:
+      return new DateFilter(options as DateFilterOptions);
 
     default:
       break;
