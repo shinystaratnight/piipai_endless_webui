aadff47d9530a62fc1fd826f157de3cd9ea2b957
Nick <nickgulya@gmail.com>
2019-03-26 11:02:49 +0200

    RV-805 - add timesheet counter

--- src/app/components/calendar/calendar.component.ts
+++ src/app/components/calendar/calendar.component.ts
@@ -64,6 +64,28 @@ export class CalendarComponent implements OnInit, OnDestroy {
   public customRange: {start: Moment, end: Moment};
   public modalInfo: any;
   public saveProcess: boolean;
+
+  public timesheetCounter = [
+    {
+      type: 1,
+      count: 0,
+      cssClass: 'text-success',
+      text: 'Filled shifts'
+    },
+    {
+      type: 0,
+      count: 0,
+      cssClass: 'text-danger',
+      text: 'Unfilled shifts'
+    },
+    {
+      type: 2,
+      count: 0,
+      cssClass: 'text-warning',
+      text: 'Pending shifts'
+    }
+  ];
+
   private modalRef: NgbModalRef;
   private lastData: any;
 
@@ -300,6 +322,12 @@ export class CalendarComponent implements OnInit, OnDestroy {
           };
         })
         .filter((shift) => this.status.data[shift.is_fulfilled]);
+
+      this.timesheetCounter.forEach((el) => el.count = 0);
+      this.shifts.forEach((shift) => {
+        const target = this.timesheetCounter.find((counter) => counter.type === shift.is_fulfilled);
+        target.count += 1;
+      });
     }
   }
 
