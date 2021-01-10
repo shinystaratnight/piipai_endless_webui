e262a0adca13be5faf65f06a55e3dbd8319cbcc2
phokke <phokke@yahoo.com>
2021-01-10 02:10:50 +0100

    RV-1041 - remove unused variable

--- libs/core/src/lib/services/site-settings.service.ts
+++ libs/core/src/lib/services/site-settings.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Renderer2 } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { map, catchError, tap } from 'rxjs/operators';
