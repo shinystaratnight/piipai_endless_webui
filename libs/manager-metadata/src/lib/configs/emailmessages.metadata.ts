c64098d96eb38bc4c17e1a9fefcf2a5b61432da9
phokke <phokke@yahoo.com>
2021-05-19 23:24:29 +0200

    RV-1159 - change subject field value

--- libs/manager-metadata/src/lib/configs/emailmessages.metadata.ts
+++ libs/manager-metadata/src/lib/configs/emailmessages.metadata.ts
@@ -51,7 +51,7 @@ const list = () => {
           name: 'id',
           label: 'ID',
         } as ColumnElement,
-        new List.column.element('subject', 'Subject').setContent([new List.text.element('template.name')]),
+        new List.column.element('subject', 'Subject').setContent([new List.text.element('subject')]),
         new List.column.element('to_addresses', 'To').setContent([new List.text.element('to_addresses')]),
         new List.column.element('state', 'State').setContent([
           new List.select.element('state').setValues({
