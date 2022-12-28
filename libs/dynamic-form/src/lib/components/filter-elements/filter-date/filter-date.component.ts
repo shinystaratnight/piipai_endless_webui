c64eca5619dfe0426744876c3036500dcae8f284
Peter Hokke <phokke@yahoo.com>
2022-12-28 02:35:29 +0200

    RV-1872 - start refactoring

--- libs/dynamic-form/src/lib/components/filter-elements/filter-date/filter-date.component.ts
+++ libs/dynamic-form/src/lib/components/filter-elements/filter-date/filter-date.component.ts
@@ -54,224 +54,240 @@ export class FilterDateComponent implements OnInit, AfterViewInit, OnDestroy {
   ) {}
 
   public ngOnInit() {
-    this.data = this.createInputs(this.config.input);
-    this.mobileDevice = isTouchDevice();
+    // this.data = this.createInputs(this.config.input);
+    // this.mobileDevice = isTouchDevice();
 
-    if (this.mobileDevice) {
-      this.displayFormat = this.queryFormat;
-    }
+    // if (this.mobileDevice) {
+    //   this.displayFormat = this.queryFormat;
+    // }
 
-    this.querySubscription = this.route.queryParams.subscribe(() => {
-      setTimeout(() => {
-        if (!(this.cd as any).destroyed) {
-          this.updateFilter();
-        }
-      }, 200);
-    });
-    this.filterSubscription = this.fs.reset.subscribe(() =>
-      this.updateFilter()
-    );
+    // this.querySubscription = this.route.queryParams.subscribe(() => {
+    //   setTimeout(() => {
+    //     if (!(this.cd as any).destroyed) {
+    //       this.updateFilter();
+    //     }
+    //   }, 200);
+    // });
+    // this.filterSubscription = this.fs.reset.subscribe(() =>
+    //   this.updateFilter()
+    // );
 
-    this.translationKey = `filter.${this.config.key}`;
+    // this.translationKey = `filter.${this.config.key}`;
   }
 
   public ngOnDestroy() {
-    this.querySubscription.unsubscribe();
-    this.filterSubscription.unsubscribe();
+    // this.querySubscription.unsubscribe();
+    // this.filterSubscription.unsubscribe();
   }
 
   public ngAfterViewInit() {
-    if (!this.init && this.d) {
-      const dateType = this.mobileDevice ? 'flipbox' : 'calbox';
-      this.init = true;
-      this.d.forEach((el: any) => {
-        (window as any).$(el.nativeElement).datebox({
-          mode: dateType,
-          dateFormat: '%d/%m/%Y',
-          overrideDateFormat: '%d/%m/%Y',
-          useClearButton: true,
-          useCancelButton: true,
-          useHeader: false,
-          useFocus: true,
-          themeDatePick: 'primary',
-          calHighToday: true,
-          overrideCalStartDay: 1,
-          theme: {
-            clearBtn: 'clear'
-          },
-          beforeOpenCallback: () => {
-            setTimeout(() => {
-              this.refreshDatebox(el.nativeElement);
-            }, 200);
-          },
-          closeCallback: () => {
-            const newDate = el.nativeElement.value || '';
-            const prevDate = this.data[el.nativeElement.name] || '';
+    // if (!this.init && this.d) {
+    //   const dateType = this.mobileDevice ? 'flipbox' : 'calbox';
+    //   this.init = true;
+    //   this.d.forEach((el: any) => {
+    //     (window as any).$(el.nativeElement).datebox({
+    //       mode: dateType,
+    //       dateFormat: '%d/%m/%Y',
+    //       overrideDateFormat: '%d/%m/%Y',
+    //       useClearButton: true,
+    //       useCancelButton: true,
+    //       useHeader: false,
+    //       useFocus: true,
+    //       themeDatePick: 'primary',
+    //       calHighToday: true,
+    //       overrideCalStartDay: 1,
+    //       theme: {
+    //         clearBtn: 'clear'
+    //       },
+    //       beforeOpenCallback: () => {
+    //         setTimeout(() => {
+    //           this.refreshDatebox(el.nativeElement);
+    //         }, 200);
+    //       },
+    //       closeCallback: () => {
+    //         const newDate = el.nativeElement.value || '';
+    //         const prevDate = this.data[el.nativeElement.name] || '';
 
-            if (newDate.trim() !== prevDate.trim()) {
-              this.onChange(newDate, el.nativeElement.name);
-            }
-          },
-        });
-        el.nativeElement.readOnly = false;
-      });
-    }
+    //         if (newDate.trim() !== prevDate.trim()) {
+    //           this.onChange(newDate, el.nativeElement.name);
+    //         }
+    //       },
+    //     });
+    //     el.nativeElement.readOnly = false;
+    //   });
+    // }
   }
 
-  public getListElementQuery(type: any) {
-    return DateFilter.element.getQuery(this.config.key, type);
-  }
+  // public getListElementQuery(type: any) {
+  //   return DateFilter.element.getQuery(this.config.key, type);
+  // }
 
-  public changeDateOnMobile() {
-    this.query = this.getQuery(this.data);
-    this.fs.generateQuery(this.query, this.config.key, this.config.listName, {
-      data: this.data,
-    });
+  // public changeDateOnMobile() {
+  //   this.query = this.getQuery(this.data);
+  //   this.fs.generateQuery(this.query, this.config.key, this.config.listName, {
+  //     data: this.data,
+  //   });
 
-    this.changeQuery();
-  }
+  //   this.changeQuery();
+  // }
 
-  public refreshDatebox(element: HTMLElement) {
-    if (element) {
-      (window as any).$(element).datebox('refresh');
-    }
-  }
+  // public refreshDatebox(element: HTMLElement) {
+  //   if (element) {
+  //     (window as any).$(element).datebox('refresh');
+  //   }
+  // }
 
-  public selectQuery(query: string) {
-    this.resetData(this.data);
+  // public selectQuery(query: string) {
+  //   this.resetData(this.data);
 
-    const params = this.getParams(query);
-    const queryParams = this.convert(undefined, this.queryFormat, params);
+  //   const params = this.getParams(query);
+  //   const queryParams = this.convert(undefined, this.queryFormat, params);
 
-    this.data = this.convert(undefined, this.displayFormat, params);
-    this.query = this.getQuery(queryParams);
+  //   this.data = this.convert(undefined, this.displayFormat, params);
+  //   this.query = this.getQuery(queryParams);
 
-    this.fs.generateQuery(this.query, this.config.key, this.config.listName, {
-      data: this.data,
-      query,
-    });
+  //   this.fs.generateQuery(this.query, this.config.key, this.config.listName, {
+  //     data: this.data,
+  //     query,
+  //   });
 
-    this.changeQuery();
+  //   this.changeQuery();
 
-    return false;
-  }
+  //   return false;
+  // }
 
-  public onChange(date: string, param: string) {
-    this.query = '';
-    this.data[param] = date;
+  // public onChange(date: string, param: string) {
+  //   this.query = '';
+  //   this.data[param] = date;
 
-    const queryParams = this.convert(
-      this.displayFormat,
-      this.queryFormat,
-      this.data
-    );
+  //   const queryParams = this.convert(
+  //     this.displayFormat,
+  //     this.queryFormat,
+  //     this.data
+  //   );
 
-    this.query = this.getQuery(queryParams);
+  //   this.query = this.getQuery(queryParams);
 
-    this.fs.generateQuery(this.query, this.config.key, this.config.listName, {
-      data: this.data,
-    });
+  //   this.fs.generateQuery(this.query, this.config.key, this.config.listName, {
+  //     data: this.data,
+  //   });
 
-    this.changeQuery();
-  }
+  //   this.changeQuery();
+  // }
 
-  public changeQuery() {
-    this.event.emit({
-      list: this.config.listName,
-    });
-  }
+  // public changeQuery() {
+  //   this.event.emit({
+  //     list: this.config.listName,
+  //   });
+  // }
 
-  public createInputs(inputs: any[]): Params {
-    const params: Record<string, any> = {};
+  // public createInputs(inputs: any[]): Params {
+  //   const params: Record<string, any> = {};
 
-    inputs.forEach((el) => {
-      params[el.query] = '';
-    });
+  //   inputs.forEach((el) => {
+  //     params[el.query] = '';
+  //   });
 
-    return params;
-  }
+  //   return params;
+  // }
 
-  public resetData(data: { [key: string]: string }) {
-    const keys = Object.keys(data);
+  // public resetData(data: { [key: string]: string }) {
+  //   const keys = Object.keys(data);
 
-    if (keys.length) {
-      keys.forEach((el) => {
-        data[el] = '';
-      });
-    }
-  }
+  //   if (keys.length) {
+  //     keys.forEach((el) => {
+  //       data[el] = '';
+  //     });
+  //   }
+  // }
 
-  public updateFilter() {
-    const data = this.fs.getQueries(this.config.listName, this.config.key);
+  // public updateFilter() {
+  //   const data = this.fs.getQueries(this.config.listName, this.config.key);
 
-    if (data) {
-      if (data.byQuery) {
-        this.query = data.query;
-        const params = this.getParams(this.query);
-        this.data = this.convert(this.queryFormat, this.displayFormat, params);
-      } else {
-        this.data = data.data;
-        this.query = data.query;
-      }
-    } else {
-      this.query = '';
-      this.resetData(this.data);
-    }
-  }
+  //   if (data) {
+  //     if (data.byQuery) {
+  //       this.query = data.query;
+  //       const params = this.getParams(this.query);
+  //       this.data = this.convert(this.queryFormat, this.displayFormat, params);
+  //     } else {
+  //       this.data = data.data;
+  //       this.query = data.query;
+  //     }
+  //   } else {
+  //     this.query = '';
+  //     this.resetData(this.data);
+  //   }
+  // }
 
-  public resetFilter() {
-    this.query = '';
-    this.resetData(this.data);
+  // public resetFilter() {
+  //   this.query = '';
+  //   this.resetData(this.data);
 
-    this.fs.generateQuery(this.query, this.config.key, this.config.listName);
-    this.changeQuery();
-  }
+  //   this.fs.generateQuery(this.query, this.config.key, this.config.listName);
+  //   this.changeQuery();
+  // }
 
-  public convert(format?: string, to?: string, params?: Params) {
-    //tslint:disable-line
-    const newParams = { ...params };
+  // public convert(format?: string, to?: string, params?: Params) {
+  //   //tslint:disable-line
+  //   const newParams = { ...params };
 
-    Object.keys(newParams).forEach((el) => {
-      newParams[el] = newParams[el]
-        ? this.parseDateValue(newParams[el], format).format(to)
-        : '';
-    });
+  //   Object.keys(newParams).forEach((el) => {
+  //     newParams[el] = newParams[el]
+  //       ? this.parseDateValue(newParams[el], format).format(to)
+  //       : '';
+  //   });
 
-    return newParams;
-  }
+  //   return newParams;
+  // }
 
-  public parseDateValue(date: string, format?: string) {
-    //tslint:disable-line
-    return format ? Time.parse(date, { format }) : Time.parse(date);
-  }
+  // public parseDateValue(date: string, format?: string) {
+  //   //tslint:disable-line
+  //   return format ? Time.parse(date, { format }) : Time.parse(date);
+  // }
 
-  public getParams(query: string): Params {
-    const params = query.split('&');
-    const result: Record<string, any> = {};
+  // public getParams(query: string): Params {
+  //   const params = query.split('&');
+  //   const result: Record<string, any> = {};
 
-    params.forEach((param) => {
-      const parts = param.split('=');
-      result[parts[0]] = parts[1];
-    });
+  //   params.forEach((param) => {
+  //     const parts = param.split('=');
+  //     result[parts[0]] = parts[1];
+  //   });
 
-    return result;
-  }
+  //   return result;
+  // }
 
-  public getQuery(params: Params): string {
-    const keys = Object.keys(params);
+  // public getQuery(params: Params): string {
+  //   const keys = Object.keys(params);
 
-    if (keys.length) {
-      const quesries = keys.map((param) => {
-        return `${param}=${params[param]}`;
-      });
+  //   if (keys.length) {
+  //     const quesries = keys.map((param) => {
+  //       return `${param}=${params[param]}`;
+  //     });
 
-      return quesries.join('&');
-    }
+  //     return quesries.join('&');
+  //   }
 
-    return '';
-  }
+  //   return '';
+  // }
 
   public getTranslateKey(type: string): string {
     return `filter.${this.config.key}.${type}`;
+  }
+
+  resetFilter() {
+
+  }
+
+  selectQuery(query: string) {
+
+  }
+
+  getListElementQuery(query: string) {
+    return false;
+  }
+
+  changeDateOnMobile() {
+
   }
 }
