a003dac875733acd47b04ac31dfbdce049ab2b20
Peter Hokke <phokke@yahoo.com>
2022-12-30 20:06:19 +0200

    RV-1924 - switch plan funationality updated

--- apps/r3sourcer/src/app/billing/components/billing-plan/billing-plan.component.ts
+++ apps/r3sourcer/src/app/billing/components/billing-plan/billing-plan.component.ts
@@ -1,4 +1,15 @@
-import { Component, EventEmitter, Output, Input, ViewChild, OnChanges, SimpleChanges, OnDestroy, ElementRef } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Output,
+  Input,
+  ViewChild,
+  OnChanges,
+  SimpleChanges,
+  OnDestroy,
+  ElementRef,
+} from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { UserService } from '@webui/core';
 
@@ -28,7 +39,19 @@ export class BillingPlanComponent implements OnChanges, OnDestroy {
   @Output() public selectedPlan = new EventEmitter();
   @Output() public cancelingPlan = new EventEmitter();
 
-  constructor(private modalService: NgbModal, private userService: UserService) {}
+  private _planControl = new FormControl(null);
+
+  constructor(
+    private modalService: NgbModal,
+    private userService: UserService
+  ) {}
+
+  get hasSelectedPlan() {
+    return (
+      this._planControl.value?.id !== this.currentPlan?.subscription_type ||
+      this.currentPlan?.worker_count !== this.workerCount
+    );
+  }
 
   get trialExpires() {
     return this.userService.user?.data.end_trial_date;
@@ -39,6 +62,14 @@ export class BillingPlanComponent implements OnChanges, OnDestroy {
       if (!changes['saveProcess'].currentValue && this.modalRef) {
         this.modalRef.close();
       }
+    }
+
+    if (changes['plans'] && changes['plans'].currentValue) {
+      this._planControl.patchValue(
+        this.plans?.find(
+          (plan) => plan.id === this.currentPlan?.subscription_type
+        )
+      );
     }
 
     this.types = {};
@@ -59,9 +90,13 @@ export class BillingPlanComponent implements OnChanges, OnDestroy {
   }
 
   public planPay(plan: Plan): number {
-    const start: number = plan.start_range_price_annual || plan.start_range_price_monthly;
+    const start: number =
+      plan.start_range_price_annual || plan.start_range_price_monthly;
 
-    const price = start + (this.workerCount - plan.start_range) * (plan.step_change_val * plan.procent);
+    const price =
+      start +
+      (this.workerCount - plan.start_range) *
+        (plan.step_change_val * plan.procent);
 
     return this.workerCount > plan.start_range ? Math.round(price) : start;
   }
@@ -73,21 +108,15 @@ export class BillingPlanComponent implements OnChanges, OnDestroy {
   }
 
   public selectPlan(plan: Plan) {
-    const body = {
-      type: plan.type,
-      worker_count: this.workerCount,
-      price: plan.type === 'monthly' ? this.planPay(plan) : this.planPayYear(plan),
-      changed: this.currentPlan,
-    };
-
-    this.selectedPlan.emit(body);
+    this._planControl.patchValue(plan);
   }
 
   public checkActivePlan(plan: Plan) {
-    if (this.currentPlan) {
-      return this.currentPlan.type === plan.type;
-    }
-    return;
+    const selectedPlan = this._planControl.value;
+
+    return selectedPlan
+      ? selectedPlan.id === plan.id
+      : this.currentPlan?.subscription_type === plan.id;
   }
 
   public setPlan() {
@@ -106,5 +135,19 @@ export class BillingPlanComponent implements OnChanges, OnDestroy {
 
   public openModal() {
     this.modalRef = this.modalService.open(this.modal, { backdrop: 'static' });
+  }
+
+  savePlan() {
+    const plan = this._planControl.value;
+
+    const body = {
+      type: plan.type,
+      worker_count: this.workerCount,
+      price:
+        plan.type === 'monthly' ? this.planPay(plan) : this.planPayYear(plan),
+      changed: this.currentPlan,
+    };
+
+    this.selectedPlan.emit(body);
   }
 }
